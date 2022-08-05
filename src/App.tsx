2f85c9b74999609b066633f51665f5c8939afe85
Sean <seanboyd_2@hotmail.com>
2022-08-05 17:55:58 +0000

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import findTransaction from './findTransaction';
+
+
+function App() {
+  return (
+    <div>
+    <h1>Payments Application</h1>
+    <><findTransaction title="Find a transaction"/>
+    </div>
+    
+  );
+}
+
+
+
+export default App;
+
+
+
